1239df9844a405a91b59cc4fe1ea9cc18a78c656
Rohit Kumar <rohitku841301@gmail.com>
2024-02-25 19:26:04 +0530

    change variable

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-
+console.log(process.env.DATABASE_HOST );
 const sequelize = new Sequelize(process.env.DATABASE_DEFAULT_NAME, process.env.DATABASE_DEFAULT_USER, process.env.DATABASE_PASSWORD, {
   dialect: "mysql",
-  origin: "localhost",
+  origin: process.env.DATABASE_HOST,
 });
 
 module.exports = sequelize;
