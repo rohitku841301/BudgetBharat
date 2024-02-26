e79eb941050b4ebd42017c8a84579eac1016997e
Rohit Kumar <rohitku841301@gmail.com>
2024-02-26 10:53:31 +0530

    update db details

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-console.log(process.env.DATABASE_HOST );
+console.log(process.env.DATABASE_DEFAULT_NAME);
 const sequelize = new Sequelize(process.env.DATABASE_DEFAULT_NAME, process.env.DATABASE_DEFAULT_USER, process.env.DATABASE_PASSWORD, {
   dialect: "mysql",
   origin: process.env.DATABASE_HOST,
