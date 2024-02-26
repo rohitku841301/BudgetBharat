89847142499b8860427d81b3810f01d0272bf133
Rohit Kumar <rohitku841301@gmail.com>
2024-02-27 01:20:23 +0530

    change in db

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 console.log(process.env.DATABASE_DEFAULT_NAME);
 const sequelize = new Sequelize(process.env.DATABASE_DEFAULT_NAME, process.env.DATABASE_DEFAULT_USER, process.env.DATABASE_PASSWORD, {
   dialect: "mysql",
-  origin: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST,
 });
 
 module.exports = sequelize;
